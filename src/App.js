7e3cb0266e5aee01aa245ba6aa0cd3b71690c845
Faadhil <faadhil.aa@gmail.com>
2021-11-21 18:25:55 -0500

    Install moment.js dependency

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import ExerciseInfo from "./Components/ExerciseInfo/ExerciseInfo";
 import SideBar from "./Components/Header/Header";
+import WeeklyCalendar from "./Components/WeeklyCalendar/WeeklyCalendar";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <SideBar>
         <ExerciseInfo></ExerciseInfo>
       </SideBar>
+    <WeeklyCalendar></WeeklyCalendar>
     </div>
   );
 }
--- src/Components/WeeklyCalendar/WeeklyCalendar.jsx
+++ src/Components/WeeklyCalendar/WeeklyCalendar.jsx
@@ -1,13 +1,27 @@
 import React from "react";
-import classNames from "classnames";
-import "bootstrap/dist/css/bootstrap.min.css";
+// import classNames from "classnames";
+// import "bootstrap/dist/css/bootstrap.min.css";
+import "react-big-calendar/lib/css/react-big-calendar.css";
 import Button from "react-bootstrap/Button";
-import "Components/WeeklyCalendar/WeeklyCalendar.css";
+// import "src/Components/WeeklyCalendar/WeeklyCalendar.css";
+
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import moment from 'moment'
+
+const localizer = momentLocalizer(moment)
 
 export default function WeeklyCalendar(props) {
   const {} = props;
   
   return (
-    <div></div>
+    <div>
+      <Calendar
+      localizer={localizer}
+      events={myEventsList}
+      startAccessor="start"
+      endAccessor="end"
+      style={{ height: 500 }}
+    />
+  </div>
   );
 }
