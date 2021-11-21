0d812ce4347457659ace8aa302518931c6bbd6f0
Faadhil <faadhil.aa@gmail.com>
2021-11-21 18:17:28 -0500

    Add big-react-calendar dependency and create calendar component

---
+++ src/Components/WeeklyCalendar/WeeklyCalendar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import classNames from "classnames";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
+import "Components/WeeklyCalendar/WeeklyCalendar.css";
+
+export default function WeeklyCalendar(props) {
+  const {} = props;
+  
+  return (
+    <div></div>
+  );
+}
